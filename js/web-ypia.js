18be6d44c340251a52ed095ee7ad14681998e85c
Pudintea <najzmitea@gmail.com>
2024-08-15 15:39:50 +0700

    Update web-ypia.js

--- js/web-ypia.js
+++ js/web-ypia.js
@@ -1,16 +1,21 @@
-// Ensure submenus toggle properly
-  document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.dropdown-submenu .dropdown-toggle').forEach(function (element) {
-      element.addEventListener('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        let parent = this.parentElement;
-        parent.querySelector('.dropdown-menu').classList.toggle('show');
-        document.querySelectorAll('.dropdown-menu').forEach(function (submenu) {
-          if (submenu !== parent.querySelector('.dropdown-menu')) {
-            submenu.classList.remove('show');
-          }
-        });
-      });
-    });
-  });
+// Berita Terbaru
+var urlPDN = 'https://najzmi.github.io/kehumasan/json/json-front-website.json';
+$.getJSON(urlPDN, function(hasil){
+	//console.log(hasil);
+	$.each(hasil, function (i, data) {
+		//console.log(data);
+		//var dadate = data.date;
+		//var hasil_dadate = toDateString(dadate);
+		$('#berita-terbaru').append(`<div class="col mb-2">
+					<div class="card h-100">
+						<img class="card-img-top" src="`+data.pudin_thumbnail_img_url+`" alt="`+data.title.rendered+`">
+						<div class="card-body">
+							<h5 class="card-title">`+data.title.rendered+`</h5>
+							<p class="card-text">Tanggal : `+data.date+`</p>
+							<a href="`+data.link+`" class="btn btn-primary" target="_blank">Kunjungi</a>
+						</div>
+					</div>
+				</div>`);
+	});
+});
+// JAVASCRIPT
